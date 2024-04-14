c0e87bf4cac659f050a6f9919f56f24005ddc29a
Gabriel Misao Pinheiro Kaminise <117327879+zKaminise@users.noreply.github.com>
2024-04-14 02:37:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// SIMPLES LOOP EM JAVA SCRIPT PARA REESCREVER ALGO USANDO ANIMACAO
+var typed = new Typed(".typing", {
+  strings: ["", "Primeiro Projeto"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
